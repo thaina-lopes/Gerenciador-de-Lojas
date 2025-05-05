e086d8b445894ccf168b51acc88582ac9a8b0c70
Thainá Lopes <thainalopes@gmail.com>
2025-05-05 15:42:58 -0300

    Etapa 2 - Associação entre classes

---
+++ Etapa2/Loja.java
@@ -0,0 +1,95 @@
+
+public class Loja {
+    //atributos 
+    private String nome;
+    private int quantidadeFuncionarios;
+    private double salarioBaseFuncionario;
+    private Endereco endereco;
+    private Data dataFundacao;
+    
+    //metodos 
+
+    //construtor1
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+        this.endereco = endereco;
+        this.dataFundacao = dataFundacao;
+    }
+    //construtor2
+    public Loja(String nome, int quantidadeFuncionarios, Endereco endereco, Data dataFundacao) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = -1;
+        this.endereco = endereco;
+        this.dataFundacao = dataFundacao;
+    }
+    
+    //get e set
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidadeFuncionarios() {
+        return quantidadeFuncionarios;
+    }
+
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+
+    public double getSalarioBaseFuncionario() {
+        return salarioBaseFuncionario;
+    }
+
+    public void setSalarioBaseFuncionario(double salarioBaseFuncionario) {
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Data getDataFundacao() {
+        return dataFundacao;
+    }
+
+    public void setDataFundacao(Data dataFundacao) {
+        this.dataFundacao = dataFundacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Loja [nome=" + nome + ", quantidadeFuncionarios=" + quantidadeFuncionarios + ", salarioBaseFuncionario="
+                + salarioBaseFuncionario + ", endereco=" + endereco + ", dataFundacao=" + dataFundacao + "]";
+    }
+
+    //método gastosComSalario
+    public double gastosComSalario() {
+        if (salarioBaseFuncionario != -1) {
+            return quantidadeFuncionarios * salarioBaseFuncionario;
+        } else {
+            return -1;
+        }
+    }
+
+    //método tamanhoDaLoja
+    public char tamanhoDaLoja() {
+        if (quantidadeFuncionarios < 10){
+            return 'P';
+        } else if (quantidadeFuncionarios >= 10 && quantidadeFuncionarios <= 30) {
+            return 'M';
+        } else {
+            return 'G';
+        }
+    }
+}
